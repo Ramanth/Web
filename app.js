95be0065405f6711074eac394c9d6f3d2a20712f
Ramanth <ramanth.addala@gmail.com>
2018-03-19 07:09:41 +0530

    adding plan ejs template and started routing to search location api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Setup of Express Framework
 var express = require('express');
 var app = express();
+var request = require('request');
 var bodyParser = require("body-parser");
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,13 +21,30 @@ app.get('/', function(req,res){
 //Navigating to Tools Used Page
 app.get('/tools-used', function(req,res){
     res.render('tools-used', {tools_used: tools_used});
-})
+});
 
+
+//Navigating to Plan of Action Page
+app.get('/plan', function(req,res){
+    res.render('plan');
+});
 
 //Navigating to Documentation Page
 app.get('/documentation', function(req,res){
     res.render('documentation');
 })
+
+
+app.post('/api-service-centers-listing', function(req,res){
+    
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=17.418185,78.344226&radius=3000&types=car_repair&keyword=bike+repair+service+car&key=AIzaSyBE2zHLDa_2pBn3CcF6YeSeA6-5gNOSCx4';
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            res.send(body);
+        }
+    })
+
+});
 
 
 app.listen(process.env.PORT, function() {
